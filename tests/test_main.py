d377cb4fcfc1e2ab02ccc71e86e0493c6fbf64f8
Adrian Vollmer <computerfluesterer@protonmail.com>
2024-05-19 11:48:12 +0200

    Add test cases for secrets regexes

--- tests/test_main.py
+++ tests/test_main.py
@@ -40,3 +40,44 @@ def test_profile_high_value_file(profile_collection):
     profile = find_matching_profile(profile_collection, "files", "SAM")
     assert profile["comment"] == "Windows user database"
     assert profile["high_value"]
+
+
+# Test all the secret regexes contained in the default profile
+@pytest.mark.parametrize(
+    "content,expected",
+    [
+        ("foo <Password><Value>thesecret</Value></Password bar", "thesecret"),
+        ("net use /user:admin thesecret x:", "thesecret"),
+        ("foo runas /user:admin thesecret", "thesecret"),
+        ("foo RunAs.exe /user:admin thesecret", "thesecret"),
+        ("foo ConvertTo-SecureString thesecret", "thesecret"),
+        ("foo password=thesecret bar", "thesecret"),
+        ("foo password = thesecret bar", "thesecret"),
+        ("----- BEGIN PRIVATE KEY -----", None),
+        ("----- BEGIN RSA PRIVATE KEY -----", None),
+        ("foo https://username:thesecret@host:80/fs bar", "thesecret"),
+        ("foo http://user:pass@hostname.com", "user:pass"),
+        ("foo https://user:pass@192.168.1.1 bar", "user:pass"),
+        ("foo ftp://user:pass@ftp.server.com bar", "user:pass"),
+        ("foo git+ssh://user:pass@ftp.server.com bar", "user:pass"),
+        ('foo cpassword="ABKRDJALKSasldkdsfa8924+sdf/fsdfk3=" bar', None),
+        ("foo <adminpass>thesecret</adminpass> bar", "thesecret"),
+        ("    password: thesecret", "thesecret"),
+        ('foo    "passwd": "thesecret"', "thesecret"),
+        ("foo Kennwort: thesecret", "thesecret"),
+        ("foo PASSWORD_SETUP=thesecret", "thesecret"),
+        ("foo PASSWORD_EIS=thesecret", "thesecret"),
+        ("foo k4AlddsflASkfwwSNdsflASkfwwSNsfl+/ASkfwwSNFslkfd2392=", None),
+        ("foo k4AlddsflASkfwwSNdsflASkfwwSNsfl+/ASkfwwSNFslkfd2392", None),
+    ],
+)
+def test_profile_secrets(content, expected, profile_collection):
+    from smbcrawler.io import find_secrets
+
+    result = find_secrets(content, profile_collection["secrets"])
+
+    assert result
+    result = result[0]
+    assert result["line"] in content
+    if expected:
+        assert expected in result["secret"]
