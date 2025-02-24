aa468252a92378abf9e39ab71fc2d11fc4d3c5e6
Adrian Vollmer <adrian.vollmer@syss.de>
2025-02-24 18:13:38 +0100

    Remove unneeded importlib_metadata import
    We don't support python 3.8.

--- src/smbcrawler/version.py
+++ src/smbcrawler/version.py
@@ -1,9 +1,4 @@
-try:
-    # importlib.metadata is present in Python 3.8 and later
-    import importlib.metadata as importlib_metadata
-except ImportError:
-    # use the shim package importlib-metadata pre-3.8
-    import importlib_metadata as importlib_metadata
+import importlib.metadata as importlib_metadata
 
 try:
     __version__ = importlib_metadata.version(__package__ or __name__)
